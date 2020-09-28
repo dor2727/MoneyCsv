1239e7cc8150b7d1e6a66f10a0ba773265634512
Dor Harpaz <harpazdor@gmail.com>
2020-09-28 21:34:47 +0300

    changed paths

--- consts.py
+++ consts.py
@@ -2,8 +2,8 @@ import os
 import re
 import datetime
 
-DEFAULT_DATA_DIRECTORY = os.path.expanduser("~/Dropbox/Projects/MoneyCsv/data")
-TELEGRAM_DATA_DIRECTORY = os.path.expanduser("~/Dropbox/Projects/MoneyCsv/Telegram_Bot/data")
+DEFAULT_DATA_DIRECTORY = os.path.expanduser("~/Projects/MoneyCsv/data")
+TELEGRAM_DATA_DIRECTORY = os.path.expanduser("~/Projects/MoneyCsv/Telegram_Bot/data")
 POSSIBLE_FILE_EXTENSIONS = ["", ".mcsv", ".csv", ".txt"]
 
 
